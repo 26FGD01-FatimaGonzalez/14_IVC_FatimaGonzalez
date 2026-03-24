24be5576501eb7b9d14afe48899847858b78c7b0
26FGD01-FatimaGonzalez <gonzalezfatima16db@gmail.com>
2026-03-24 09:50:00 -0600

    Add files via upload
    Actividad1_Leccion7_incs1

---
+++ Actividad1_Leccion7_incs1.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("==PROMEDIO DE CALIFICACIONES==");
+        //DECLARACION DE VARIABLES
+        string materia;
+        int n1, n2, n3, n4, n5;
+        double promedio;
+        //ENTRADA DE DATOS
+        Console.Write("Ingrese el nombre de la materia: ");
+        materia = Console.ReadLine();
+        Console.Write("Ingrese 5 CALIFICACIONES: ");
+        n1 = Convert.ToInt32(Console.ReadLine());
+        n2 = Convert.ToInt32(Console.ReadLine()); 
+        n3 = Convert.ToInt32(Console.ReadLine()); 
+        n4 = Convert.ToInt32(Console.ReadLine());
+        n5 = Convert.ToInt32(Console.ReadLine());
+        //PROCESO
+        promedio = (n1 + n2 + n3 + n4 + n5) / 5;
+        //SALIDA DE DATOS, EVALUAR SI GANO O PERDIO
+        if(promedio >= 70)
+            Console.WriteLine("El promedio es: " +promedio+" y el estudiante APROBO");
+        else
+            Console.WriteLine("El promedio es: " + promedio + " y el estudiante REPROBO");
+    }
+}

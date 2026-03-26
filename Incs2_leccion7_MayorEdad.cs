f058dce09c71b8f054244806ab7f1eefe0f04579
26FGD01-FatimaGonzalez <gonzalezfatima16db@gmail.com>
2026-03-26 08:25:59 -0600

    Add files via upload

---
+++ Incs2_leccion7_MayorEdad.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int edad;
+        Console.WriteLine("Ingrese su edad:");
+        edad = int.Parse(Console.ReadLine());
+
+        if (edad >= 18)
+            Console.WriteLine("¡¡TU ERES MAYOR DE EDAD!!");
+        else
+            Console.WriteLine("TU ERES MENOR DE EDAD");
+        
+        
+    }
+}

6767c88ba7a51f3b9e15516dcc4b580dca41be39
26FGD01-FatimaGonzalez <gonzalezfatima16db@gmail.com>
2026-03-26 21:46:04 -0600

    Add files via upload

---
+++ Act2_leccion7_incs2.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int valorNota;
+
+        Console.Write("Ingrese la nota: ");
+        valorNota = Convert.ToInt32(Console.ReadLine());
+
+        if (valorNota < 0 || valorNota > 100)
+        {
+            Console.WriteLine("Nota inválida");
+        }
+        else if (valorNota < 70)
+        {
+            Console.WriteLine("Deficiente :(");
+        }
+        else if (valorNota < 80)
+        {
+            Console.WriteLine("Regular");
+        }
+        else if (valorNota < 85)
+        {
+            Console.WriteLine("Bien!!");
+        }
+        else if (valorNota < 95)
+        {
+            Console.WriteLine("¡Muy bien!");
+        }
+        else
+        {
+            Console.WriteLine("EXCELENTE");
+        }
+    }
+}

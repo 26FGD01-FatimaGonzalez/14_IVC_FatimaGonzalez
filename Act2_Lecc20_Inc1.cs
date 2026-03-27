7cfdf2fb78a75a2cd00bf5b045207e03bf9f262f
26FGD01-FatimaGonzalez <gonzalezfatima16db@gmail.com>
2026-03-27 17:39:14 -0600

    Add files via upload

---
+++ Act2_Lecc20_Inc1.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string password;
+
+        Console.Write("Ingrese su contraseña: ");
+        password = Console.ReadLine();
+
+        if (password == "Password123")
+        {
+            Console.WriteLine("Bienvenida");
+        }
+        else
+        {
+            Console.WriteLine("El ingreso está prohibido");
+        }
+    }
+}
